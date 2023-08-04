aadf9e3c317a9d103e6bd3ac40d5f1e1839b5472
Jeandre Visser <jeandre@nutrimeals.ca>
2023-08-03 21:53:22 -0600

    Function for getting the count of of leaked hashed passwords

--- checkmypass.py
+++ checkmypass.py
@@ -8,12 +8,20 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}, check API and try again.')
     return res
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    print(hashes)
+    for h, count in hashes:
+        print(h, count)
+        if h == hash_to_check:
+            return count
+    return 0
+
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
-    print(first5_char, tail)
     print(response)
-    return response
+    return get_password_leaks_count(response, tail)
 
 pwned_api_check('123')
